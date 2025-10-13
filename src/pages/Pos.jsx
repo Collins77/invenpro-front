9b0311cca07b7a363a4defbdb17ce32aaf19802c
Collins77 <collinsmuemah@gmail.com>
2025-10-13 18:05:06 +0300

    load

--- src/pages/Pos.jsx
+++ src/pages/Pos.jsx
@@ -296,7 +296,7 @@ const Pos = () => {
                             <button disabled={confirmingSale} className='py-2 px-4 w-full cursor-pointer border border-gray-300 rounded-md hover:bg-black/60 hover:text-white transition-all duration-300' onClick={resetCart}>
                                 Reset
                             </button>
-                            <button disabled={confirmingSale} className='py-2 px-4 w-full cursor-pointer border border-gray-300 rounded-md bg-black text-white' onClick={handleConfirmSale}>
+                            <button disabled={confirmingSale} className='py-2 px-4 w-full cursor-pointer border border-gray-300 rounded-md bg-black text-white flex items-center gap-1' onClick={handleConfirmSale}>
                                 {confirmingSale && <Loader className='animate-spin' size={18} />}
                                 {confirmingSale ? 'Processing...' : 'Confirm Sale'}
                             </button>
